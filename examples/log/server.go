77e6bd01dae4f626ec25cdf5953c0bf9d3691ddf
Pedro Barco <pedro.barco@tecnico.ulisboa.pt>
2023-03-26 22:28:23 +0100

    feat: add tracing to examples

--- examples/log/server.go
+++ examples/log/server.go
@@ -2,22 +2,18 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"go.uber.org/zap"
 )
 
 type server struct {
 	quoteController *quoteController
-	logger          *zap.SugaredLogger
 	engine          *gin.Engine
 }
 
 func NewServer(
 	quoteController *quoteController,
-	logger *zap.SugaredLogger,
 ) *server {
 	return &server{
 		quoteController: quoteController,
-		logger:          logger,
 		engine:          gin.New(),
 	}
 }
