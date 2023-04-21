628fb8099ab6b45fcf2c753819052f9ad17a38c7
Pedro Barco <pedro.barco@tecnico.ulisboa.pt>
2023-04-21 01:38:03 +0100

    docs: add comments to health package

--- pkg/health/health.go
+++ pkg/health/health.go
@@ -5,6 +5,7 @@ import (
 	"github.com/hellofresh/health-go/v5/checks/http"
 )
 
+// NewServerComponent creates a health component for a server
 func NewServerComponent(name, version string) health.Component {
 	return health.Component{
 		Name:    name,
@@ -12,6 +13,7 @@ func NewServerComponent(name, version string) health.Component {
 	}
 }
 
+// NewHTTPDSHealthCheck creates a health check config for an HTTP datasource
 func NewHTTPDSHealthCheck(name, url string) health.Config {
 	return health.Config{
 		Name: name,
--- pkg/health/srvplugin.go
+++ pkg/health/srvplugin.go
@@ -10,11 +10,15 @@ const (
 	livezPath  = "/livez"
 )
 
+// WithServer adds a live check endpoint and ready check endpoint to a given
+// server
 func WithServer(srv *gin.Engine, component health.Component, checks ...health.Config) {
 	srv.GET(livezPath, newLivezHandler(component))
 	srv.GET(readyzPath, newReadyzHandler(component, checks...))
 }
 
+// newLivezHandler creates a live check. This check is used to check if the
+// server is available and alive
 func newLivezHandler(component health.Component) gin.HandlerFunc {
 	h, err := health.New(
 		health.WithComponent(component),
@@ -27,6 +31,8 @@ func newLivezHandler(component health.Component) gin.HandlerFunc {
 	return gin.WrapH(h.Handler())
 }
 
+// newReadyzHandler creates a ready check. This check is used to check if the
+// server is ready to use and serve the traffic.
 func newReadyzHandler(component health.Component, checks ...health.Config) gin.HandlerFunc {
 	h, err := health.New(
 		health.WithComponent(component),
